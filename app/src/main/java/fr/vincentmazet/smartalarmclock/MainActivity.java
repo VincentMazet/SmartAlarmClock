bd6a19a3b95e384226481474f51e0c2feb36922c
Marimax <maxime.ducoroy01@gmail.com>
2017-05-04 17:32:18 +0200

    merge

--- app/src/main/java/fr/vincentmazet/smartalarmclock/MainActivity.java
+++ app/src/main/java/fr/vincentmazet/smartalarmclock/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
